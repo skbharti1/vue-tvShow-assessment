82509dfd3db9575a04db404801cf95fb74a131b1
Saurabh Bharti <saurabhkrbharti@gmail.com>
2020-07-20 11:35:41 +0530

    code cleanup

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
-    <v-container :justify="center" :align="center">
+    <v-container :style="{ textAlign: 'center' }">
+      <br />
+      <br />
       <h1>TV Show Application</h1>
+      <br />
+      <br />
       <p>
         This application is built to display the details of TV shows and movies.
         This web application is built using Vue, VueRouter, Axios and Vuetify.
